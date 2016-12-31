0001bb4c5c642c96bf127c2224e3407daa8daae3
sampanna pokhrel <sampanna99@gmail.com>
2016-12-31 13:25:38 -0800

    refactored upto separation of concern

---
+++ ThirdTime/Controllers/GigsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+using ThirdTime.Models;
+using ThirdTime.ViewModels;
+
+namespace ThirdTime.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel { Genres = _context.Genres.ToList() };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            // var artist = _context.Users.Single(u => u.Id == artistId);
+            //var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                ArtistId = artistId,
+                DateTime = viewModel.DateTime,
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
+        }
+
+    }
+}
