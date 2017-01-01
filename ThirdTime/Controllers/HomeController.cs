5a90fb2e43f9bbb113c0956fd4a3007f07a3191f
sampanna pokhrel <sampanna99@gmail.com>
2016-12-31 23:38:17 -0800

    precise and beautiful design

--- ThirdTime/Controllers/HomeController.cs
+++ ThirdTime/Controllers/HomeController.cs
@@ -1,16 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using ThirdTime.Models;
 
 namespace ThirdTime.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs.Include(a => a.Artist).Include(g => g.Genre).Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
--- ThirdTime/Models/AccountViewModels.cs
+++ ThirdTime/Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace ThirdTime.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
