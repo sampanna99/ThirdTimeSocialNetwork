c029db3e86599952f7d09353497a3ab7963e7d6a
sampanna pokhrel <sampanna99@gmail.com>
2016-12-31 17:49:37 -0800

    Client side validation

---
+++ ThirdTime/ViewModels/FutureTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace ThirdTime.ViewModels
+{
+    public class FutureTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                  "HH:mm",
+                  CultureInfo.CurrentCulture,
+                  DateTimeStyles.None, out dateTime);
+            return (isValid);
+        }
+
+    }
+}
